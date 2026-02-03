9b5af82e0d35dff62234e4eed62a3c55dd2d9baf
Divya Philip <divyaphilip@Divyas-MacBook-Air.local>
2026-02-04 01:04:19 +0530

    add day one program

--- day01/first_non_repeating_char.py
+++ day01/first_non_repeating_char.py
@@ -1,7 +1,35 @@
-import os
+"""
+    Return the first character in string s that does not repeat.
+    If none exists, return None.
+    """
 
-def first_non_rep_char():
-    pass
 
-if __name__="__main__":
-    first_non_rep_char()
+def first_non_rep_char(s):
+    freq={}
+    for char in s:
+        freq[char]=freq.get(char,0)+1
+
+    for char in s:
+        if freq[char] == 1:
+            return char
+    return None
+
+
+if __name__=="__main__":
+    s = input("Enter a string: \n")
+    print(first_non_rep_char(s))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
